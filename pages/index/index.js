295709d0f5b10bbab769a22222773f3c90991000
hfylyg <8154275822@qq.com>
2018-10-07 17:28:11 +0800

    hfy

---
+++ pages/index/index.js
@@ -0,0 +1,80 @@
+// pages/index/index.js
+const start="郑州";
+const finish="西峡";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    start:start,
+    finish:finish,
+    month:9,
+    date:23,
+    week:"三"
+  },
+  //车站查询
+  tiao:function(e){
+   
+  },
+  tran:function(e){
+    
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/list/list.js
@@ -0,0 +1,31 @@
+// pages/list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    month:9,
+    date:26,
+    week:"三",
+    time:"06:30",
+    price:20,
+    residue:10,
+    start:"郑州中心站",
+    finish:"晋城客运东站",
+    array:[1,2,3,4,5]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
